221ac5d2994d55da6a0ad1847f177f9327125206
Lenka Horvathova <lenka.m.horvathova@gmail.com>
2020-02-27 18:20:33 +0100

    Initial Commit

---
+++ HW1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW1
+//
+//  Created by Lenka Horvathova on 27/02/2020.
+//  Copyright © 2020 FI MU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
